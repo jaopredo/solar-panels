74186b00ea33c2010dd550985c7de63f34af16b7
João Pedro <joao.profissional.45@gmail.com>
2023-02-03 16:07:35 -0300

    Terminado região sobre

--- src/components/Areas/About.area.jsx
+++ src/components/Areas/About.area.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from 'react'
+
+import "./About.area.scss"
 
 /* IMAGES */
 import { FiPhoneCall } from 'react-icons/fi'
@@ -6,29 +8,31 @@ import OldEngineer from '../../images/old-engineer.jpg'
 import YoungEngineer from '../../images/young-engineer.jpg'
 
 function About() {
-    return <>
-        <article>
-            <div>
+    return <section id="about-section">
+        <article id="phone-article">
+            <div id="phone-article-container">
                 <h1>Solar Solutions</h1>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores totam odio minima hic. Sed voluptatem distinctio eligendi quod earum similique fuga quibusdam tempora, quas cupiditate praesentium esse. Voluptatem, est a?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores totam odio minima hic. Sed voluptatem distinctio eligendi quod
                 </p>
                 <button><FiPhoneCall/>CALL NOW</button>
             </div>
             <img src={OldEngineer} alt="engenheiro velho" />
         </article>
-        <article>
-            <img src={YoungEngineer} alt='engenheiro jovem' />
-            <section>
-                <h2>We Have a Solar Plan for Almost Any Budget</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, blanditiis labore quasi</p>
-            </section>
-            <section>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, esse. Officia consectetur, aut quam neque, nisi doloremque ea atque ipsum optio, odit impedit? Quo dolore, similique a ea voluptatum recusandae.</p>
-                <a href="#">MORE</a>
-            </section>
+        <article id="budget-article">
+            <div id="budget-article-container">
+                <img src={YoungEngineer} alt='engenheiro jovem' />
+                <section id="budget-article-section-1">
+                    <h2>We Have a Solar Plan<br/>for Almost Any<br/>Budget</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, blanditiis labore quasi</p>
+                </section>
+                <section id="budget-article-section-2">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, esse. Officia consectetur, aut quam neque, nisi doloremque ea atque ipsum optio, odit impedit? Quo dolore, similique a ea voluptatum recusandae.</p>
+                    <a href="#">MORE</a>
+                </section>
+            </div>
         </article>
-    </>
+    </section>
 }
 
 export default About;
